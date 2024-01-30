3e4cf0e55319b1967b7b4ef045c132331c57042f
dmitrii <polkovnikov13@gmail.com>
2024-01-30 21:53:59 +0100

    refactoring

--- app.go
+++ app.go
@@ -3,25 +3,5 @@ package main
 import "learn_words/gui"
 
 func main() {
-	app := gui.NewGuiApplication()
-	println(app)
-	//myApp := app.New()
-	//myWindow := myApp.NewWindow("Learn words")
-	//myWindow.Resize(fyne.Size{
-	//	Width:  300,
-	//	Height: 300,
-	//})
-	//
-	//groupSelector := widget.NewCheckGroup(common.Groups.GetAllGroups(), func(strings []string) {})
-	//startBtn := widget.NewButton("Run check", func() {
-	//	words := common.Groups.GetWords(groupSelector.Selected)
-	//	if len(words) == 0 {
-	//		dialog.NewError(errors.New("список слов пуст"), myWindow).Show()
-	//	} else {
-	//		window := gui.NewShowWordsWindow(myApp, words.Shuffle(10))
-	//		window.Show()
-	//	}
-	//})
-	//myWindow.SetContent(container.NewVBox(groupSelector, startBtn))
-	//myWindow.ShowAndRun()
+	gui.NewGuiApplication()
 }
