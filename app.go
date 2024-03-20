f543a1799c5fcc846b850c0bf7d7044d70d18699
Дмитрий <Polkovnikov13@gmail.com>
2024-03-20 23:05:46 +0100

    Add new words (#36)
    * Add new words
    Add new version of words
    Add function to convert between formats
    
    * Add flag to convert words

--- app.go
+++ app.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fyne.io/fyne/v2/app"
 	"learn_words/gui"
 )
 
 func main() {
-	gui.NewApplication(
-		app.NewWithID("com.dp9v.lingua-learn"),
-	)
+	convertFlag := flag.Bool("convert", false, "Set this flag to perform conversion")
+	flag.Parse()
+
+	if *convertFlag {
+		Convert()
+	} else {
+		gui.NewApplication(
+			app.NewWithID("com.dp9v.lingua-learn"),
+		)
+	}
+
 }
